dce8e5518cb703d4b0b434a7c6f7efcb462b3936
UdlaAriel <ariel.anchapaxi@gmail.com>
2025-04-23 16:53:27 -0500

    Sounds FX and animation added to the project for the footsteeps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 	public float speed;
-
+	public Animator animator;
 	private Vector2 movement;
 
 	private Rigidbody2D rb;
 	private PlayerInput input;
 
 	private void Start()
-	{
+	{	
+		speed = 4;
 		rb = GetComponent<Rigidbody2D>();
 		input = GetComponent<PlayerInput>();
 	}
@@ -19,6 +20,25 @@ public class PlayerMovement : MonoBehaviour
 	void Update()
 	{
 		movement = input.actions["Move"].ReadValue<Vector2>();
+		animator.SetFloat("movement",movement.magnitude * speed);
+
+		if (movement.magnitude > 0.1f)
+		{
+			if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+			{
+				if (movement.x > 0)
+					transform.localScale = new Vector3(1,1,1);
+				else
+					transform.localScale = new Vector3(-1,1,1);
+			}
+			else
+			{
+				if (movement.y > 0)
+					Debug.Log("Arriba");
+				else
+					Debug.Log("Abajo");
+			}
+		}
 	}
 
 	private void FixedUpdate()
