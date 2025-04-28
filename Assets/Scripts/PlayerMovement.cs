e394d8412e47ff8ec900a8008f8a8090cd4d772e
UdlaAriel <ariel.anchapaxi@gmail.com>
2025-04-28 15:50:37 -0500

    Adding textures to the sprites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed;
-	public Animator animator;
+	[SerializeField] private GameObject player;
+	[SerializeField] private GameObject candle;
+	[SerializeField] private float speed;
+	private Animator playerAnimator;
 	private Vector2 movement;
-
+	private SpriteRenderer candleSprite;
+	private SpriteRenderer playerSprite;
 	private Rigidbody2D rb;
 	private PlayerInput input;
 
 	private void Start()
 	{	
+		playerSprite = player.GetComponent<SpriteRenderer>();
+		candleSprite = candle.GetComponent<SpriteRenderer>();
+		playerAnimator = player.GetComponent<Animator>();
+		
 		speed = 4;
 		rb = GetComponent<Rigidbody2D>();
 		input = GetComponent<PlayerInput>();
@@ -20,16 +27,25 @@ public class PlayerMovement : MonoBehaviour
 	void Update()
 	{
 		movement = input.actions["Move"].ReadValue<Vector2>();
-		animator.SetFloat("movement",movement.magnitude * speed);
+		playerAnimator.SetFloat("movement",movement.magnitude * speed);
 
 		if (movement.magnitude > 0.1f)
 		{
 			if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
 			{
 				if (movement.x > 0)
-					transform.localScale = new Vector3(1,1,1);
+				{
+					playerSprite.flipX = false;
+					candleSprite.flipX = true;
+					candle.transform.position = new Vector2(-0.15f, 0.225f);
+				}
 				else
-					transform.localScale = new Vector3(-1,1,1);
+				{
+					playerSprite.flipX = true;
+					candleSprite.flipX = false;
+					candle.transform.position = new Vector2(0.15f, 0.225f);
+				}
+					
 			}
 			else
 			{
